18637d0a6040074471c142bd680e3e5f9bab0cdc
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2025-11-18 15:24:37 +0200

    minor fix after create_project

--- core/controllers/project.py
+++ core/controllers/project.py
@@ -9,9 +9,11 @@ def get_current_project(user='admin'):
     global CURRENT_PROJECTS
     return CURRENT_PROJECTS.get(user, '')
 
+
 def set_current_project(user='admin', project='project_1'):
     global CURRENT_PROJECTS
     CURRENT_PROJECTS[user] = project
+
 
 def create_project(project, df):
     project_dir = PROJECTS_DIR / project
