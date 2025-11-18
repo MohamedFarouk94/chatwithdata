66137471251c361da2595db8a67dd6557a4930e0
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2025-11-18 15:24:02 +0200

    create_project

--- core/controllers/project.py
+++ core/controllers/project.py
@@ -1,4 +1,7 @@
+import json
+from pathlib import Path
 from core.utils.paths import PROJECTS_DIR
+
 
 CURRENT_PROJECTS = {}
 
@@ -10,5 +13,20 @@ def set_current_project(user='admin', project='project_1'):
     global CURRENT_PROJECTS
     CURRENT_PROJECTS[user] = project
 
-def create_project():
-    pass
+def create_project(project, df):
+    project_dir = PROJECTS_DIR / project
+    project_dir.mkdir(parents=True, exist_ok=True)
+
+    models_dir = project_dir / 'ai_generated_models'
+    models_dir.mkdir(parents=True, exist_ok=True)
+
+    plots_dir = project_dir / 'ai_generated_plots'
+    plots_dir.mkdir(parents=True, exist_ok=True)
+
+    df.to_csv(project_dir / 'df.csv', index=False)
+
+    with open(project_dir / 'models.json', 'w') as f:
+        json.dump([], f, indent=4)
+
+    with open(project_dir / 'plots.json', 'w') as f:
+        json.dump([], f, indent=4)
