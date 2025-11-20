9862f80ac26e7a41ad136c3a5678a5450982bcd7
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2025-11-20 14:40:55 +0200

    update executer

--- core/components/executer.py
+++ core/components/executer.py
@@ -2,7 +2,7 @@ from langchain_core.runnables import RunnableLambda
 from core.utils.exec import run_agent_code
 
 def execute(data):
-    code_res = run_agent_code(data['code'])
+    code_res = run_agent_code(data['code'], data['project'])
     data.update(code_res)
     data['activate_prompt'] = 2
     return data
--- core/utils/exec.py
+++ core/utils/exec.py
@@ -1,5 +1,4 @@
 import io, contextlib, traceback
-
 import re
 import numpy as np
 import pandas as pd
@@ -11,6 +10,8 @@ import json
 import sklearn.metrics as metrics
 from sklearn.model_selection import train_test_split
 from catboost import CatBoostClassifier, CatBoostRegressor
+from core.utils.ai import add_model, add_plot
+from core.utils.paths import get_project_directory
 
 
 namespace = {
@@ -24,14 +25,15 @@ namespace = {
     'CatBoostRegressor': CatBoostRegressor,
     'json': json,
     'time': time,
-    # 'add_model': add_model, # LATER
-    # 'add_plot': add_plot, # LATER
+    'add_model': add_model,
+    'add_plot': add_plot,
     'train_test_split': train_test_split,
     'metrics': metrics
 }
 
-def run_agent_code(code, work_dir=''):
+def run_agent_code(code, project):
     buf = io.StringIO()
+    namespace['project_dir'] = get_project_directory(project)
     try:
         with contextlib.redirect_stdout(buf), contextlib.redirect_stderr(buf):
             exec(code, namespace, namespace)
