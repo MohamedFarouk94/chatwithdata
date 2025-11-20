96a473700563a88c3d2eb735c504fcef7a6d1efa
Mohamed Farouk <mohamedfarouk1994@gmail.com>
2025-11-20 13:28:11 +0200

    create get_project_directory function

--- core/utils/paths.py
+++ core/utils/paths.py
@@ -3,3 +3,7 @@ from pathlib import Path
 
 CORE_DIR = Path(__file__).resolve().parent.parent
 PROJECTS_DIR = CORE_DIR / "projects"
+
+def get_project_directory(project):
+    global PROJECTS_DIR
+    return PROJECTS_DIR / project
